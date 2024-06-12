a0d008e0305ff23f7173e7ba38a8776f00de5ced
aidanar <aidanarfer@outlook.es>
2024-06-12 19:44:41 +0200

    add endpoints

--- bd/pets.sql
+++ bd/pets.sql
@@ -6,21 +6,20 @@ CREATE TABLE pets (
     name VARCHAR(45) not null,
     species VARCHAR(45) not null,
     sex VARCHAR(45) not null,
-    isAlive boolean,
     descr VARCHAR(255) not null
 );
 
-INSERT INTO `memorablepets`.`pets` (`name`, `species`, `sex`, `isAlive`, `descr`) VALUES ('Luka', 'perro', 'h', '0', 'El perrete más chiquitín del mundo. Vivía por y para su mamá (y la comida).');
-INSERT INTO `memorablepets`.`pets` (`name`, `species`, `sex`, `isAlive`, `descr`) VALUES ('Niko', 'hamster', 'm', '0', 'Un hamster con muy mala leche.');
-INSERT INTO `memorablepets`.`pets` (`name`, `species`, `sex`, `isAlive`, `descr`) VALUES ('Lola', 'ninfa', 'h', '0', 'Se escapó en nochevieja, la muy desagradecida.');
-INSERT INTO `memorablepets`.`pets` (`name`, `species`, `sex`, `isAlive`, `descr`) VALUES ('Pepa', 'perro', 'h', '0', 'Pepita linda, era un angelito.');
-INSERT INTO `memorablepets`.`pets` (`name`, `species`, `sex`, `isAlive`, `descr`) VALUES ('Nala', 'perro', 'h', '1', 'La mamá de Luka, tiene mucho aguante.');
-INSERT INTO `memorablepets`.`pets` (`name`, `species`, `sex`, `isAlive`, `descr`) VALUES ('Gaia', 'gato', 'h', '0', 'Es un ser perfecto.');
-INSERT INTO `memorablepets`.`pets` (`name`, `species`, `sex`, `isAlive`, `descr`) VALUES ('Señor Dorotencio', 'tortuga', 'm', '0', 'Le gustaba esconderse detrás de los armarios.');
-INSERT INTO `memorablepets`.`pets` (`name`, `species`, `sex`, `isAlive`, `descr`) VALUES ('Tata', 'perro', 'h', '1', 'Tatita bebé, amante de los tequeños. Es un perrito langosta.');
-INSERT INTO `memorablepets`.`pets` (`name`, `species`, `sex`, `isAlive`, `descr`) VALUES ('Macarena', 'perro', 'h', '1', 'Tiene orejas preciosas y una voz muy aguda.');
-INSERT INTO `memorablepets`.`pets` (`name`, `species`, `sex`, `isAlive`, `descr`) VALUES ('Nikito', 'hamster', 'm', '0', 'El hamster de mi prima. Se copió del nombre del mío.');
-INSERT INTO `memorablepets`.`pets` (`name`, `species`, `sex`, `isAlive`, `descr`) VALUES ('Dana', 'cobaya', 'h', '0', 'Le daban unos espasmos un poco raros.');
-INSERT INTO `memorablepets`.`pets` (`name`, `species`, `sex`, `isAlive`, `descr`) VALUES ('Estrella', 'perro', 'h', '0', 'Era más lista que el hambre.');
-INSERT INTO `memorablepets`.`pets` (`name`, `species`, `sex`, `isAlive`, `descr`) VALUES ('Huevo pelao', 'perro', 'm', '0', 'No sabemos su nombre real.');
-INSERT INTO `memorablepets`.`pets` (`name`, `species`, `sex`, `isAlive`, `descr`) VALUES ('Mosqui', 'mosca', 'm', '1', 'Cualquier mosca puede ser mosqui.');
+INSERT INTO `memorablepets`.`pets` (`name`, `species`, `sex`, `descr`) VALUES ('Luka', 'perro', 'h', 'El perrete más chiquitín del mundo. Vivía por y para su mamá (y la comida).');
+INSERT INTO `memorablepets`.`pets` (`name`, `species`, `sex`, `descr`) VALUES ('Niko', 'hamster', 'm', 'Un hamster con muy mala leche.');
+INSERT INTO `memorablepets`.`pets` (`name`, `species`, `sex`, `descr`) VALUES ('Lola', 'ninfa', 'h', 'Se escapó en nochevieja, la muy desagradecida.');
+INSERT INTO `memorablepets`.`pets` (`name`, `species`, `sex`, `descr`) VALUES ('Pepa', 'perro', 'h', 'Pepita linda, era un angelito.');
+INSERT INTO `memorablepets`.`pets` (`name`, `species`, `sex`, `descr`) VALUES ('Nala', 'perro', 'h', 'La mamá de Luka, tiene mucho aguante.');
+INSERT INTO `memorablepets`.`pets` (`name`, `species`, `sex`, `descr`) VALUES ('Gaia', 'gato', 'h', 'Es un ser perfecto.');
+INSERT INTO `memorablepets`.`pets` (`name`, `species`, `sex`, `descr`) VALUES ('Señor Dorotencio', 'tortuga', 'm', 'Le gustaba esconderse detrás de los armarios.');
+INSERT INTO `memorablepets`.`pets` (`name`, `species`, `sex`, `descr`) VALUES ('Tata', 'perro', 'h', 'Tatita bebé, amante de los tequeños. Es un perrito langosta.');
+INSERT INTO `memorablepets`.`pets` (`name`, `species`, `sex`, `descr`) VALUES ('Macarena', 'perro', 'h', 'Tiene orejas preciosas y una voz muy aguda.');
+INSERT INTO `memorablepets`.`pets` (`name`, `species`, `sex`, `descr`) VALUES ('Nikito', 'hamster', 'm', 'El hamster de mi prima. Se copió del nombre del mío.');
+INSERT INTO `memorablepets`.`pets` (`name`, `species`, `sex`, `descr`) VALUES ('Dana', 'cobaya', 'h', 'Le daban unos espasmos un poco raros.');
+INSERT INTO `memorablepets`.`pets` (`name`, `species`, `sex`, `descr`) VALUES ('Estrella', 'perro', 'h', 'Era más lista que el hambre.');
+INSERT INTO `memorablepets`.`pets` (`name`, `species`, `sex`, `descr`) VALUES ('Huevo pelao', 'perro', 'm', 'No sabemos su nombre real.');
+INSERT INTO `memorablepets`.`pets` (`name`, `species`, `sex`, `descr`) VALUES ('Mosqui', 'mosca', 'm', 'Cualquier mosca puede ser mosqui.');
--- src/index.js
+++ src/index.js
@@ -30,24 +30,102 @@ async function getConnection() {
 // ENDPOINTS
 
 // get (toda la tabla)
-app.get("/pets", async () => {
+app.get("/pets", async (req, res) => {
     try {
         const conn = await getConnection();
         const select = 'SELECT * FROM pets;';
         const [results] = await conn.query(select);
 
-        results.status(200).json({
+        res.status(200).json({
             success: true,
             count: results.length,
             data: results
         })
     } catch (error) {
-        results.status(400).json({
+        res.status(400).json({
             sucess: false,
             error: error
         })
     }
-})
+});
 
 // get (datos filtrados)
-app.get("/pets/:id")
+app.get("/pets/:id", async (req, res) => {
+
+    try {
+        const id = req.params.id;
+        const conn = await getConnection();
+        const select = 'SELECT * FROM pets WHERE id = ?;';
+        const [results] = await conn.query(select, [id]);
+    
+        if (results.length === 0) {
+            res.status(400).json({success: false, message: "No existe una mascota con ese id"});
+        } else {
+            res.status(200).json({success: true, data: results[0]});
+        }
+    } catch (error) {
+        res.status(400).json({success: false, message: error});
+    }  
+});
+
+// post (añadir)
+app.post("/addPet", async (req, res) => {
+    const conn = await getConnection();
+    const { name, species, sex, descr } = req.body;
+   
+    const insert = "INSERT into pets (name, species, sex, descr) values(?,?,?,?)";
+    const [newPet] = await conn.query(insert, [
+        name,
+        species,
+        sex,
+        descr,
+    ]);
+
+    res.status(200).json({
+        success: true,
+        id: newPet.insertId
+    });
+
+    await conn.end();
+});
+
+// put (modificar)
+app.put("/pets/:id", async (req, res) => {
+    const conn = await getConnection();
+    const idPet = req.params.id;
+    const { name, species, sex, descr } = req.body;
+
+    const update = "UPDATE pets SET name = ?, species = ?, sex = ?, descr = ? WHERE id = ?;";
+    const [results] = await conn.query(update, [
+        name,
+        species,
+        sex,
+        descr,
+        idPet
+    ]);
+
+    console.log(req.body)
+
+    if (results.affectedRows > 0) {
+        res.status(200).json({success: true, newData: req.body });
+    } else {
+        res.status(200).json({success: false, message: "No existe una mascota con ese id"})
+    }
+});
+
+// delete
+app.delete("/pets/:id", async (req, res) => {
+    const conn = await getConnection();
+    const idPet = req.params.id;
+    const deletePet = "DELETE from pets WHERE id = ?;";
+    const [results] = await conn.query(deletePet, [idPet]);
+
+    console.log(idPet);
+
+    if (results.affectedRows > 0) {
+        res.status(200).json({success: true, message: "Registro borrado" });
+    } else {
+        res.status(200).json({success: false, message: "No existe una mascota con ese id"})
+    }
+
+})
